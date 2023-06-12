e93a5eb61c43cdbf1fcdeba9d3ed28645fae0649
skyno <samir.abis@outlook.it>
2023-06-12 20:53:16 +0200

    first working build! fixed folder bug

--- app/clipboard_monitor.py
+++ app/clipboard_monitor.py
@@ -5,19 +5,24 @@ from io import BytesIO
 
 
 def start_clipboard_monitor(drive_uploader):
-    recent_value = ""
+    recent_image = None
     while True:
-        tmp_value = pyperclip.paste()
-        if tmp_value != recent_value:
-            recent_value = tmp_value
-            try:
-                im = ImageGrab.grabclipboard()  # get image from clipboard
-                if im is not None:
-                    byte_data = BytesIO()
-                    im.save(byte_data, format="PNG")
-                    drive_uploader.upload_file(
-                        byte_data.getvalue(), "screenshot.png"
-                    )  # upload to google drive
-            except Exception as e:
-                print(e)  # log the error
+        try:
+            im = ImageGrab.grabclipboard()  # get image from clipboard
+            if im is not None and not compare_images(im, recent_image):
+                recent_image = im
+                byte_data = BytesIO()
+                im.save(byte_data, format="PNG")
+                drive_uploader.upload_file(
+                    byte_data.getvalue(), "screenshot.png"
+                )  # upload to google drive
+                byte_data.close()  # close the BytesIO object
+        except Exception as e:
+            print(e)  # log the error
         time.sleep(0.1)  # pause to reduce cpu usage
+
+
+def compare_images(img1, img2):
+    if img1 is None or img2 is None:
+        return False
+    return list(img1.getdata()) == list(img2.getdata())
--- app/drive_uploader.py
+++ app/drive_uploader.py
@@ -1,6 +1,11 @@
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
+from googleapiclient.http import MediaIoBaseUpload
+from io import BytesIO
 from dotenv import load_dotenv
+import tempfile
+import datetime
+import time
 import os
 
 load_dotenv()
@@ -9,9 +14,13 @@ load_dotenv()
 class DriveUploader:
     def __init__(self):
         gauth = GoogleAuth()
+        gauth.LoadClientConfigFile(
+            "app/client_secrets.json"
+        )  # Replace with the full path to your client_secrets.json file
         gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication
         self.drive = GoogleDrive(gauth)
         self.folder_id = self.find_or_create_folder("Snippets")
+        self.last_temp_file = None
 
     def find_or_create_folder(self, folder_name):
         """Find a folder with the given name, or create it if it doesn't exist."""
@@ -28,8 +37,53 @@ class DriveUploader:
             return folder["id"]
 
     def upload_file(self, file_data, file_name):
-        file = self.drive.CreateFile(
-            {"title": file_name, "parents": [{"id": self.folder_id}]}
+        """
+        Upload a file to Google Drive.
+        :param file_data: The data of the file to be uploaded in bytes.
+        :param file_name: The name of the file to be uploaded.
+        """
+
+        # Create a temporary file and write the byte data to it
+        temp = tempfile.mkstemp(suffix=".png")
+        os.close(temp[0])  # Close the file descriptor
+        temp_name = temp[1]
+        with open(temp_name, "wb") as f:
+            f.write(file_data)
+
+        # If there's a previous temp file, attempt to delete it now
+        if self.last_temp_file is not None:
+            try:
+                os.unlink(self.last_temp_file)
+                print(
+                    f"Previous temporary file {self.last_temp_file} successfully deleted."
+                )
+            except Exception as e:
+                print(
+                    f"Failed to delete previous temporary file {self.last_temp_file}: {e}"
+                )
+            finally:
+                self.last_temp_file = None
+
+        # Get the current time and format it as a string
+        now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+        # Use the current time to create a unique file name
+        file_name = f"screenshot_{now}.png"
+
+        # Check if folder still exists, if not, create it
+        self.folder_id = self.find_or_create_folder("Snippets")
+
+        # Create a new file and set its content
+        new_file = self.drive.CreateFile(
+            {
+                "title": file_name,
+                "parents": [{"id": self.folder_id}],
+                "mimeType": "image/png",
+            }
         )
-        file.SetContentString(file_data)
-        file.Upload()
+        new_file.SetContentFile(temp_name)
+        new_file.Upload()
+        print("File uploaded with ID: ", new_file.get("id"))
+
+        # Instead of deleting the temp file, keep track of it for next time
+        self.last_temp_file = temp_name
